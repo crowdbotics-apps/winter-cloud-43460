12463303102b9b73cb9dfa3301670d6ce34762ef
Crowdbotics <team@crowdbotics.com>
2023-09-19 19:25:48 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "winter-cloud-43460",
+  url: "https://winter-cloud-43460.botics.co",
+  api: "https://winter-cloud-43460.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
